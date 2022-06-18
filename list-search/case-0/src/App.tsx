ce9da3479c34ea43dabb900b471a0c50a4765a6e
fmothe <mothevv.13@gmail.com>
2022-06-18 00:28:47 -0300

    case_0 finalizado

--- list-search/case-0/src/App.tsx
+++ list-search/case-0/src/App.tsx
@@ -7,24 +7,33 @@ import api from "./api";
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [cargando, setCargando] = useState<boolean>(false);
 
   useEffect(() => {
-    api.search(query);
+    setCargando(true);
+    api.search(query).then((response) => {
+      setCargando(false);
+      setProducts(response);
+    });
   }, [query]);
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
-            <span>$ {product.price}</span>
-          </li>
-        ))}
-      </ul>
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      {cargando ? (
+        <p>Cargando...</p>
+      ) : (
+        <ul>
+          {products.map((product) => (
+            <li key={product.id} className={product.price < 100 ? "sale" : ""}>
+              <h4>{product.title}</h4>
+              <p>{product.description}</p>
+              <span>$ {product.price}</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
